660db8a218521d5f57eda46490880faa5b116e60
Nora Muhammad <noramohamedfcis@gmail.com>
2021-05-05 22:34:30 +0200

    Kwalee integrated

---
+++ Assets/Kwalee/Scripts/Analytics.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+using Facebook.Unity;
+
+public class Analytics : MonoBehaviour
+{
+    private void Awake()
+    {
+        if(Application.platform == RuntimePlatform.IPhonePlayer)
+        {
+            Application.targetFrameRate = 60;
+        }
+
+        FB.Init(FBInitCallback);
+    }
+
+    private void FBInitCallback()
+    {
+        if(FB.IsInitialized)
+        {
+            FB.ActivateApp();
+        }
+    }
+
+    public void OnApplicationPause(bool pause)
+    {
+        if(!pause)
+        {
+            if(FB.IsInitialized)
+            {
+                FB.ActivateApp();
+            }
+        }
+    }
+}
