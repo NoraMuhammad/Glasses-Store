8e0ab347533fd550d51a125eaa6e5167acc26978
Nora Muhammad <noramohamedfcis@gmail.com>
2021-05-09 01:46:31 +0200

    UI for 1080x1350 resolution

--- Assets/Scripts/GameStrings.cs
+++ Assets/Scripts/GameStrings.cs
@@ -2,12 +2,12 @@
 
 public class GameStrings : MonoBehaviour
 {
-    public static string NextUnlockString = "__NextUnlockStringggggg__";
-    public static string UpgradedPotIndexString = "__UpgradedPotIndexStringggggg__";
-    public static string MoneyString = "__MoneyStringggggg__";
-    public static string SceneIndexString = "__SceneIndexStringggggg__";
-    public static string DayIndexString = "__DayIndexStringggggg__";
-    public static string PlantPotUpgrade = "__PlantPotUpgradeStringggggg__";
-    public static string PlantPotShown = "__PlantPotShownStringggggg__";
-    public static string TutorialString = "__TutorialStringggggg__";
+    public static string NextUnlockString = "__NextUnlockStringggggggggg__";
+    public static string UpgradedPotIndexString = "__UpgradedPotIndexStringggggggggg__";
+    public static string MoneyString = "__MoneyStringggggggggg__";
+    public static string SceneIndexString = "__SceneIndexStringgggggggg__";
+    public static string DayIndexString = "__DayIndexStringggggggggg__";
+    public static string PlantPotUpgrade = "__PlantPotUpgradeStringggggggggg__";
+    public static string PlantPotShown = "__PlantPotShownStringggggggggg__";
+    public static string TutorialString = "__TutorialStringggggggggg__";
 }
